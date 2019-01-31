73f91826c05e5c3c40a6519e06cb2ffaeec44780
Adam Saladino <adam@codingsimply.com>
2019-01-31 10:46:31 -0500

    setup packages. added sample index usages.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,11 @@
 // @flow
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+import IndexController from 'site-index/lib/Controller/IndexController';
+import IndexArgs from 'site-index/lib/Model/Args';
+import FileDetails from 'site-index/lib/Model/FileDetails';
+
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
@@ -9,12 +15,35 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
-  render() {
-    return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </div>
-    );
-  }
+  startIndex = () => {
+    const indexArgs = new IndexArgs({
+      domain: 'codingsimply.com',
+      output: new FileDetails(
+        'C:\\Users\\Adam\\Documents\\programming\\reports\\'
+      )
+    });
+    console.log(indexArgs);
+    const indexController = new IndexController(indexArgs);
+    indexController
+      .start((event, progress) => {
+        console.log(event, progress);
+      })
+      .then(() => {
+        console.log('finished');
+        return true;
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  };
+
+  render = () => (
+    <div className={styles.container} data-tid="container">
+      <h2>Home</h2>
+      <Link to={routes.COUNTER}>to Counter</Link>
+      <Button onClick={this.startIndex} variant="contained" type="button">
+        Start Scan
+      </Button>
+    </div>
+  );
 }
