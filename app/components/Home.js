b53d3535e982caf94486f53c6d4e6eea5bc53f47
Adam Saladino <adam@codingsimply.com>
2019-02-01 17:02:47 -0500

    Added basic UI and indexing capabilities.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,49 +1,157 @@
 // @flow
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import { Grid, LinearProgress, Button, TextField } from '@material-ui/core';
+import { FolderOpen } from '@material-ui/icons';
 
 import IndexController from 'site-index/lib/Controller/IndexController';
 import IndexArgs from 'site-index/lib/Model/Args';
 import FileDetails from 'site-index/lib/Model/FileDetails';
 
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
+import electron from 'electron';
+
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
 
-export default class Home extends Component<Props> {
+type Progress = {
+  url: { url: string },
+  urls: number,
+  urlsPool: number
+};
+
+export default class Home extends Component<Props, Progress> {
   props: Props;
 
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      domain: '',
+      output: '',
+      overallCompleted: 0,
+      overallTotal: 6,
+      overStatus: '',
+      taskCompleted: 0,
+      taskTotal: 100,
+      taskStatus: ''
+    };
+    this.startIndex = this.startIndex.bind(this);
+    this.setDomain = this.setDomain.bind(this);
+  }
+
   startIndex = () => {
+    const { domain, output } = this.state;
+    this.setState({ overStatus: `Indexing ${domain}` });
     const indexArgs = new IndexArgs({
-      domain: 'codingsimply.com',
-      output: new FileDetails(
-        'C:\\Users\\Adam\\Documents\\programming\\reports\\'
-      )
+      domain,
+      output: new FileDetails(output),
+      verbose: false
     });
-    console.log(indexArgs);
     const indexController = new IndexController(indexArgs);
     indexController
-      .start((event, progress) => {
-        console.log(event, progress);
+      .start((event, progress: Progress) => {
+        if (progress) {
+          this.setState({
+            taskStatus: `${progress.url.url}`,
+            taskTotal: progress.urls + progress.urlsPool,
+            taskCompleted: progress.urls
+          });
+        }
       })
       .then(() => {
-        console.log('finished');
+        this.setState({
+          overStatus: `Done indexing ${domain}`,
+          taskStatus: '',
+          taskTotal: 100,
+          taskCompleted: 100
+        });
         return true;
       })
       .catch(() => {
-        console.log('error');
+        this.setState({ overStatus: `Error indexing ${domain}` });
       });
   };
 
-  render = () => (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
-      <Button onClick={this.startIndex} variant="contained" type="button">
-        Start Scan
-      </Button>
-    </div>
-  );
+  outputFolder = () => {
+    const output = electron.remote.dialog.showOpenDialog({
+      properties: ['openDirectory']
+    });
+    if (output) {
+      this.setState({ output: output[0] });
+    }
+  };
+
+  setDomain = (event: any) => {
+    this.setState({
+      domain: event.target.value
+    });
+  };
+
+  render = () => {
+    const {
+      output,
+      overallCompleted,
+      overallTotal,
+      overStatus,
+      taskCompleted,
+      taskTotal,
+      taskStatus
+    } = this.state;
+    return (
+      <div className={styles.container} data-tid="container">
+        <Grid container spacing={16}>
+          <Grid item xs={12}>
+            <h2>Scan a site</h2>
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              fullWidth
+              type="text"
+              onChange={this.setDomain}
+              label="Domain"
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              fullWidth
+              type="text"
+              value={output}
+              label="Output Folder"
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              onClick={this.outputFolder}
+              variant="contained"
+              type="button"
+            >
+              <FolderOpen />
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button onClick={this.startIndex} variant="contained" type="button">
+              Start Scan
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <div>{overStatus}</div>
+            <LinearProgress
+              variant="determinate"
+              value={overallCompleted}
+              valueBuffer={overallTotal}
+            />
+            <br />
+            <LinearProgress
+              color="secondary"
+              variant="determinate"
+              value={taskCompleted}
+              valueBuffer={taskTotal}
+            />
+            <div>{taskStatus}</div>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  };
 }
